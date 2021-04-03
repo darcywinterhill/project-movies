a2e8a820dab467bf00417e50fdce35cc4ace8282
[Darcy Winterhill] <[darcy.winterhill@gmail.com]>
2021-04-03 10:21:17 +0200

    commit changed back-button hover to padding

--- code/src/reusables/urls.js
+++ code/src/reusables/urls.js
@@ -1,5 +1,6 @@
 const API_KEY = 'e90c1322becf61268c0c0fea570c7099';
 export const MOVIE_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
-export const DETAILS_URL = (id) => `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US&page=1`
+// API to be used in MovieDetails
+// export const DETAILS_URL = (id) => `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US&page=1`
 
